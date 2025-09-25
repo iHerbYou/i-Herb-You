aa42bc7780da0833b41b7eb93f08f9f7ecd9fa5f
shawnchoi8 <bychoise8@gmail.com>
2025-09-25 18:36:10 +0900

    feat: Enable JWT authentication in Swagger UI

--- src/main/java/com/iherbyou/config/OpenApiConfig.java
+++ src/main/java/com/iherbyou/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package com.iherbyou.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +23,16 @@ public class OpenApiConfig {
                 .servers(List.of(
                         new Server().url("https://www.iherbyou.store").description("Production"),
                         new Server().url("http://localhost:8080").description("Local")
-                ));
+                ))
+                // JWT 인증 설정
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
     }
+
 }
