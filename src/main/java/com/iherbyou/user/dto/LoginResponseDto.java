a45c8264fb8f4357fb11e5bc4edd5f1a0972dc54
shawnchoi8 <bychoise8@gmail.com>
2025-09-28 16:01:49 +0900

    feat: add refresh token fields to LoginResponseDto

--- src/main/java/com/iherbyou/user/dto/LoginResponseDto.java
+++ src/main/java/com/iherbyou/user/dto/LoginResponseDto.java
@@ -11,9 +11,11 @@ public class LoginResponseDto { //TODO
 
     private String email;
     private String name;
-    private String accessToken; // JWT Token
+    private String accessToken; // JWT Access Token
+    private String refreshToken; // JWT Refresh Token
     private String tokenType; // Bearer
-    private Long expiresIn; // 토큰 만료 시간 (초)
+    private Long accessTokenExpirationIn; // Access Token 만료 시간 (초)
+    private Long refreshTokenExpirationIn;  // Refresh Token 만료 시간 (초)
     private String message;
 
 }
