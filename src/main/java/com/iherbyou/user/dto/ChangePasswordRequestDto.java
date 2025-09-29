c39e42118af2cd3dbd4192e19eb6ddb035e4b547
shawnchoi8 <bychoise8@gmail.com>
2025-09-29 17:20:22 +0900

    fix: password maximum length setting (x -> 20)

--- src/main/java/com/iherbyou/user/dto/ChangePasswordRequestDto.java
+++ src/main/java/com/iherbyou/user/dto/ChangePasswordRequestDto.java
@@ -15,7 +15,7 @@ public class ChangePasswordRequestDto {
     private String currentPassword;
 
     @NotBlank(message = "새 비밀번호 입력은 필수입니다.")
-    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다")
     private String newPassword;
 
 }
